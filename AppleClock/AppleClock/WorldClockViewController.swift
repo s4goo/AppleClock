eec13fa3bc24c1119156b6d86f53637d2aeb0080
Kevin Sun <s4goo.main@gmail.com>
2025-04-29 14:02:35 -0800

    스토리보드 세계 시계 탭 및 뷰컨트롤러 구성

---
+++ AppleClock/AppleClock/WorldClockViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WorldClockViewController.swift
+//  AppleClock
+//
+//  Created by Gunwoo Sun on 4/29/25.
+//
+
+import UIKit
+
+class WorldClockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
